94d3acb8a2d5dd06be5cb59110248ef93f65b161
ondax <31773208+ondax@users.noreply.github.com>
2024-10-30 23:11:51 +0100

    Přidejte soubory projektu.

---
+++ CommonUtilities/EventHandlers.cs
@@ -0,0 +1,26 @@
+﻿using Interactables.Interobjects.DoorUtils;
+using InventorySystem.Items;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Events;
+
+namespace CommonUtilities
+{
+    public class EventHandlers
+    {
+        [PluginEvent]
+        public void OnPlayerInteractDoor(PlayerInteractDoorEvent ev)
+        {
+            if(Plugin.Singleton.Config.RemoteKeycardEnabled)
+            {
+                foreach (ItemBase item in ev.Player.Items)
+                {
+                    if(ev.Door.RequiredPermissions.CheckPermissions(item, ev.Player.ReferenceHub))
+                    {
+                        DoorEvents.TriggerAction(ev.Door, ev.Door.IsConsideredOpen() ? DoorAction.Closed : DoorAction.Opened , ev.Player.ReferenceHub);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CommonUtilities/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using PluginAPI.Core.Attributes;
+using PluginAPI.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonUtilities
+{
+    public class Plugin
+    {
+        [PluginConfig]
+        public Config Config;
+        public static Plugin Singleton { get; private set; }
+        [PluginPriority(PluginAPI.Enums.LoadPriority.Medium)]
+        [PluginEntryPoint("CommonUtilities", "1.0", "Common stuff", "ondax")]
+        void LoadPlugin()
+        {
+            Singleton = this;
+            EventManager.RegisterEvents<EventHandlers>(this);
+               
+        }
+    }
+}
